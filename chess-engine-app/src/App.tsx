8226758a224b4fc54eea5733e53593be3fd35db6
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-06-19 14:10:56 +0200

    init

---
+++ chess-engine-app/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chessboard from './ChessBoard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      
+
+      </header>
+
+      <Chessboard />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ chess-engine-app/src/ChessBoard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ChessBoard.css';
+
+
+const ChessBoard : React.FC = () => {
+
+
+
+    return (
+        <div className='chessboard'>
+
+
+        </div>
+
+
+    );
+}
+
+
+export default ChessBoard;
