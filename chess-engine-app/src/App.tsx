5e08de3df5fcc7591aabea2fc13d206038c41368
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-06-19 17:21:40 +0200

    fixes

--- chess-engine-app/src/App.tsx
+++ chess-engine-app/src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Chessboard from './ChessBoard';
+import Chessboard from './Chessboard';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="chessboard-container">
 
-      
-
-      </header>
 
       <Chessboard />
 
--- chess-engine-app/src/ChessBoard.tsx
+++ chess-engine-app/src/ChessBoard.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import './ChessBoard.css';
+import './Chessboard.css';
 
 
-const ChessBoard : React.FC = () => {
+const Chessboard : React.FC = () => {
 
-
-
+    const rows = Array(8).fill(null);  // An array with 8 elements to represent 8 rows
+    const cols = Array(8).fill(null);
     return (
-        <div className='chessboard'>
+      <div className="chessboard">
+        {
+            rows.map((row, rowIndex) => (
+                <div className="chessboard-row" key={rowIndex}>
+                    {
+                        cols.map((col, colIndex) => (
+                            <div className={((rowIndex + colIndex) % 2 === 0 ? 'white' : 'black')+'-cell'} key={colIndex}>
+                                
+                            </div>
 
+                        )
+                    )
+                    }
+                </div>
 
-        </div>
+            )
+            )}
+      </div>
 
 
     );
 }
 
 
-export default ChessBoard;
+export default Chessboard;
